659765db47e4fd35c46e3aa41b30274c3179f5a1
l7-dev <81977103+l7-dev@users.noreply.github.com>
2021-04-15 03:05:12 +0900

    Add files via upload

--- js/jquery.js
+++ js/jquery.js
@@ -128,5 +128,40 @@
 		}
 	});
 	/*PWA 종료*/
+	
+	/*f12 & 우클릭 막기*/
+	var keydownCtrl = 0;
+	var keydownShift = 0;
+
+	document.onkeydown = keycheck;
+	document.onkeyup = uncheckCtrlShift;
+
+	function keycheck() {
+		switch (event.keyCode) {
+			case 123:
+				event.keyCode = '';
+				return false;
+				break; //F12
+			case 17:
+				event.keyCode = '';
+				keydownCtrl = 1;
+				return false;
+				break; //컨트롤키
+		}
+		if (keydownCtrl) return false;
+	}
+
+	function uncheckCtrlShift() {
+		if (event.keyCode == 17) keydownCtrl = 0;
+		if (event.keyCode == 16) keydownShift = 0;
+	}
+
+	function click() {
+		if ((event.button == 2) || (event.button == 2)) {
+			alert('마트랑계산기 :: 마트갈때 필수품');
+		}
+	}
+	document.onmousedown = click;
+	/*f12 & 우클릭 막기 종료*/
 
   });
